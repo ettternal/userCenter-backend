bc8510b9e6d8e6799c80a81eb95fcbecc848531c
qhyang <2194784330@qq.com>
2023-11-19 00:46:57 +0800

    基本功能完结状态

---
+++ src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.yupi.usercenter.mapper;
+
+import com.yupi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
